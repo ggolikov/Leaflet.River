0ef32d9c5bc1fdf0f3870335b7058572bd3f26f8
ggolikov <gr.golikov@gmail.com>
2017-07-08 23:35:39 +0300

    Update demo@

--- src/L.River.js
+++ src/L.River.js
@@ -8,25 +8,24 @@
 
 L.River = L.FeatureGroup.extend({
     options: {
+        color: 'blue',
         minWidth: 1,
-        maxWidth: 5
+        maxWidth: 10,
+        ratio: null
     },
 
     initialize: function (latLngs, options) {
         L.FeatureGroup.prototype.initialize.call(this, [], options);
         this._latLngs = latLngs;
 
-        L.Util.setOptions(this, options);
-
+        L.setOptions(this, options);
         this._buildLines(latLngs);
     },
 
     onAdd: function (map) {
         L.FeatureGroup.prototype.onAdd.call(this, map);
-
         this._getLength(map);
-
-        this._setStyle();
+        this.setStyle();
     },
 
     _buildLines: function (latLngs) {
@@ -36,7 +35,7 @@ L.River = L.FeatureGroup.extend({
             this.addLayer(line);
         }
     },
-    // ll's
+
     _getLength: function (map) {
         var latLngs = this._latLngs,
             totalLength = 0;
@@ -47,19 +46,9 @@ L.River = L.FeatureGroup.extend({
 
         return this._length = totalLength;
     },
-    // points
-    // _getLength: function (map) {
-    //     var totalLength = 0;
-    //
-    //     this._points = this._latLngs.map(function (ll) {return map.latLngToLayerPoint(ll)});
-    //
-    //     for (var i = 0; i < this._points.length - 1; i++) {
-    //         totalLength += this._points[i].distanceTo(this._points[i+1]);
-    //     }
-    //
-    //     return this._length = totalLength;
-    // },
-    _setStyle: function (style) {
+
+    /* pubic interface */
+    setStyle: function (style) {
         this.options = L.extend(this.options, style);
 
         var opt = this.options,
@@ -73,35 +62,31 @@ L.River = L.FeatureGroup.extend({
         for (var key in layers) {
             layer = layers[key];
             latLngs = layer.getLatLngs();
-            // points = latLngs.map(function (ll) {return map.latLngToLayerPoint(ll)});
             length += map.distance(latLngs[0], latLngs[1]);
-            // length += points[0].distanceTo(points[1]);
 
             var percent = length / totalLength;
             var w = opt.minWidth + (opt.maxWidth * percent);
 
             layer.setStyle(L.extend({}, opt, {
-                // color: 'black',
-                weight: opt.minWidth + (opt.maxWidth - opt.minWidth) * percent
+                weight: opt.ratio ? length / opt.ratio : opt.minWidth + (opt.maxWidth - opt.minWidth) * percent
             }));
         }
     },
 
-    /* pubic interface */
     setMinWidth: function (width) {
-        this._setStyle({
+        this.setStyle({
             minWidth: width
         })
     },
 
     setMaxWidth: function (width) {
-        this._setStyle({
+        this.setStyle({
             maxWidth: width
         })
-        console.log('min');
-        console.log(this.options.minWidth);
-        console.log('max');
-        console.log(this.options.maxWidth);
+    },
+
+    useLength: function (ratio) {
+        L.setOptions(this, {ratio: ratio});
     }
 });
 
